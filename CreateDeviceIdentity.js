7a053aba448f9f688f832509674ca45026f134b8
Prabhas Harlapur <prabhas.harlapur@gmail.com>
2017-05-21 02:08:34 +0530

    Added CreateDeviceIdentity.js

---
+++ CreateDeviceIdentity.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+var iothub =require('azure-iothub');
+
+var connectionString = '{HostName=poc-prabhas-iot-hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=GwYeub2mrFAR88uw0PbdgxbNOU/eJ5k4PVMsgTwZht8=}';
+
+var registry = iothub.Registry.fromConnectionString(connectionString);
+
+
+var device = new iothub.Device(null);
+device.deviceId = 'myFirstNodeDevice';
+
+
+registry.create(device, function(err, deviceInfo, res) {
+  if (err) {
+    registry.get(device.deviceId, printDeviceInfo);
+  }
+  if (deviceInfo) {
+    printDeviceInfo(err, deviceInfo, res)
+  }
+});
+
+function printDeviceInfo(err, deviceInfo, res) {
+  if (deviceInfo) {
+    console.log('Device ID: ' + deviceInfo.deviceId);
+    console.log('Device key: ' + deviceInfo.authentication.symmetricKey.primaryKey);
+  }
+}
+
